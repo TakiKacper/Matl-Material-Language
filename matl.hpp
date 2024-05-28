51eb0aa4705e5ee273d025a96a82d0fb895996ad
KacperOdzimek <kacper-odzimek@wp.pl>
2024-05-29 00:34:00 +0200

    Moved a lot of functions from previous version

--- matl.hpp
+++ matl.hpp
@@ -81,11 +81,23 @@ namespace matl
 
 namespace matl_internal
 {
-	void matl_throw(std::string error);
+	enum class exception_type
+	{
+		unexpected_end_of_line,
+		unknown_keyword,
+
+		symbol_declaration,
+		invalid_syntax,
+
+		invalid_scalar_literal,
+		mismatched_parentheses
+	};
+
+	void matl_throw(exception_type type, std::vector<std::string>&& sub_errors);
 
 	class matl_exception : public std::exception
 	{
-	friend void matl_throw(std::string error);
+	friend void matl_throw(exception_type type, std::vector<std::string>&& sub_errors);
 
 	private:
 		std::string message;
@@ -97,10 +109,29 @@ namespace matl_internal
 		}
 	};
 
-	void matl_throw(std::string error)
+	void matl_throw(exception_type type, std::vector<std::string>&& sub_errors)
 	{
+		std::string message;
+
+		switch (type)
+		{
+		case matl_internal::exception_type::unexpected_end_of_line:
+			message = "Unexpected end of line"; break;
+		case matl_internal::exception_type::unknown_keyword:
+			message = "Unknown keyword"; break;
+		case matl_internal::exception_type::symbol_declaration:
+			message = "Symbols cannot be declared in material"; break;
+		case matl_internal::exception_type::invalid_syntax:
+			message = "Invalid syntax"; break;
+		case matl_internal::exception_type::invalid_scalar_literal:
+			message = "Invalid scalar literal"; break;
+		}
+
+		for (auto& error : sub_errors)
+			message += "\n\t" + error;
+
 		auto exc = matl_exception{};
-		exc.message = std::move(error);
+		exc.message = std::move(message);
 		throw exc;
 	}
 }
@@ -135,6 +166,11 @@ namespace matl_internal
 		size_t size() const
 		{
 			return end - begin;
+		}
+
+		const char& at(size_t id)
+		{
+			return source.at(begin + id);
 		}
 
 		friend bool operator == (const string_ref& q, const std::string& other);
@@ -229,7 +265,7 @@ namespace matl_internal
 		}
 	};
 
-	struct solver
+	struct hgm_solver
 	{
 		static inline bool equal(const std::string& a, const std::string& b)
 		{
@@ -274,6 +310,11 @@ namespace matl_internal
 		return (is_at_source_end(source, iterator) || source.at(iterator) == '\n');
 	};
 
+	inline const char& get_char(const std::string& source, size_t& iterator)
+	{
+		return source.at(iterator++);
+	}
+
 	inline int get_spaces(const std::string& source, size_t& iterator)
 	{
 		int spaces = 0;
@@ -309,7 +350,7 @@ namespace matl_internal
 		}
 
 		if (iterator == begin)
-			matl_throw("Unexpected end of line; Expected token");
+			matl_throw(exception_type::unexpected_end_of_line, {});
 
 		return string_ref(source, begin, iterator);
 	}
@@ -409,6 +450,47 @@ namespace matl_internal
 				std::vector<valid_types_set> _allowed_types)
 				: symbol(_symbol), operation_display_name(_operation_display_name),
 				precedence(_precedence), allowed_types(_allowed_types) {};
+		};
+
+		struct expression
+		{
+			struct node
+			{
+				enum class node_type
+				{
+					single_arg_left_parenthesis,
+					scalar_literal,
+					variable,
+					symbol,
+					unary_operator,
+					binary_operator,
+					vector_contructor_operator,
+					vector_component_access,
+					function,
+
+				} type;
+
+				union node_value
+				{
+					std::string						scalar_value;
+					std::string						variable_name;
+					std::string						symbol_name;
+					const unary_operator*			unary_operator;
+					const binary_operator*			binary_operator;
+					uint8_t							vector_size;
+					std::vector<uint8_t>			included_vector_components;
+					std::pair<std::string, int>		function_name_and_passed_args;
+					~node_value() {};
+				} value;
+			};
+
+			std::list<node*> nodes;
+
+			~expression()
+			{
+				for (auto& n : nodes)
+					delete n;
+			}
 		};
 	}
 }
@@ -523,13 +605,243 @@ namespace matl_internal
 		const matl_internal::math::data_type* scalar_data_type = get_data_type({ "scalar" });
 
 		const char comment_char = '#';
+		const char symbols_prefix = '$';
+
+		const uint8_t functions_precedence = 3;
 	}
 }
 
+/*
+=======================================
+	MATL TYPE DEFINITIONS
+=======================================
+*/
+
+namespace matl_internal
+{
+	namespace typedefs
+	{
+		struct variable_definition
+		{
+			const math::data_type* return_type;
+			math::expression definition;
+		};
+
+		struct property_definition
+		{
+			math::expression definition;
+		};
+	}
+}
 
 /*
 =======================================
-	PARSE IMPLEMENTATION
+	EXPRESSIONS PARSING
+=======================================
+*/
+
+namespace matl_internal
+{
+	namespace expressions_parsing_internal
+	{
+		string_ref get_node_str(const std::string& source, size_t& iterator)
+		{
+			if (is_operator(source.at(iterator)))
+			{
+				iterator++;
+				return string_ref{ source, iterator - 1, iterator };
+			}
+
+			size_t begin = iterator;
+
+			bool only_digits = true;
+			bool dot_used = false;
+
+			while (true)
+			{
+				const char& c = source.at(iterator);
+
+				if (!isdigit(c) && c != '.') only_digits = false;
+
+				if (is_operator(c))
+				{
+					if (c != '.') break;
+					if (!only_digits) break;
+					if (dot_used) break;
+					dot_used = true;
+				}
+
+				if (is_whitespace(c)) break;
+
+				iterator++; if (iterator == source.size()) break;
+			}
+
+			if (iterator == begin)
+				matl_throw(exception_type::unexpected_end_of_line, {});
+
+			return string_ref{ source, begin, iterator };
+		}
+
+		bool is_function_call(const std::string& source, size_t& iterator)
+		{
+			get_spaces(source, iterator);
+			return (source.at(iterator) == '(');
+		}
+
+		bool is_scalar_literal(const std::string& node_str)
+		{
+			int dot_pos = -1;
+
+			for (int i = 0; i < node_str.size(); i++)
+				if (node_str.at(i) == '.' && dot_pos == -1)
+					dot_pos = i;
+				else if (!isdigit(node_str.at(i)))
+					return false;
+
+			if (dot_pos == 0 || dot_pos == node_str.size() - 1)
+				matl_throw(exception_type::invalid_scalar_literal, { node_str });
+
+			return true;
+		}
+
+		bool is_unary_operator(const std::string& node_str)
+		{
+			if (node_str.size() != 1) return false;
+			return get_unary_operator(node_str.at(0)) != nullptr;
+		}
+
+		bool is_binary_operator(const std::string& node_str)
+		{
+			if (node_str.size() != 1) return false;
+			return get_binary_operator(node_str.at(0)) != nullptr;
+		}
+
+		int get_comas_inside_parenthesis(const std::string& source, size_t iterator)
+		{
+			int parenthesis_deepness = 0;
+			int comas = 0;
+
+			while (!is_at_line_end(source, iterator))
+			{
+				switch (source.at(iterator))
+				{
+				case '(':
+					parenthesis_deepness++; break;
+				case ')':
+					parenthesis_deepness--;
+					if (parenthesis_deepness == 0)
+						goto _get_comas_inside_parenthesis_break;
+					break;
+				case ',':
+					if (parenthesis_deepness == 1)
+						comas++;
+					break;
+				}
+				iterator++;
+			}
+
+		_get_comas_inside_parenthesis_break:
+			if (parenthesis_deepness != 0)
+				matl_throw(exception_type::mismatched_parentheses, {});
+
+			return comas;
+		}
+
+		int get_precedence(const math::expression::node* node)
+		{
+			switch (node->type)
+			{
+			case math::expression::node::node_type::function:
+				return builtins::functions_precedence;
+			case math::expression::node::node_type::binary_operator:
+				return node->value.binary_operator->precedence;
+			case math::expression::node::node_type::unary_operator:
+				return node->value.unary_operator->precedence;
+			case math::expression::node::node_type::vector_component_access:
+				return 256;
+			}
+
+			return 0;
+		}
+
+		bool is_any_of_left_parentheses(const math::expression::node* node)
+		{
+			return	node->type == math::expression::node::node_type::vector_contructor_operator ||
+					node->type == math::expression::node::node_type::single_arg_left_parenthesis ||
+					node->type == math::expression::node::node_type::function;
+		}
+
+		void insert_operator(
+			std::list<math::expression::node*>& operators_stack, 
+			std::list<math::expression::node*>& output,
+			math::expression::node* node
+		)
+		{
+			while (operators_stack.size() != 0)
+			{
+				auto previous = operators_stack.back();
+				if (
+					previous->type != math::expression::node::node_type::single_arg_left_parenthesis &&
+					previous->type != math::expression::node::node_type::vector_contructor_operator &&
+					get_precedence(previous) >= get_precedence(node)
+					)
+				{
+					output.push_back(previous);
+					operators_stack.pop_back();
+				}
+				else break;
+			}
+
+			operators_stack.push_back(node);
+		}
+
+		void shunting_yard(
+			math::expression::node*& new_node,
+			std::list<math::expression::node*>& output,
+			std::list<math::expression::node*>& operators,
+			const std::string& source,
+			size_t& iterator
+		);
+	}
+
+	math::expression get_expression(const std::string& source, size_t& iterator)
+	{
+		math::expression::node* new_node = nullptr;
+
+		std::list<math::expression::node*> output;
+		std::list<math::expression::node*> operators;
+
+		try
+		{
+			expressions_parsing_internal::shunting_yard(new_node, output, operators, source, iterator);
+		}
+		catch (const matl_exception& exc)
+		{
+			if (new_node != nullptr) delete new_node;
+
+			for (auto& n : output)
+				delete n;
+
+			for (auto& n : operators)
+				delete n;
+
+			throw exc;
+		}
+
+		math::expression exp;
+		exp.nodes = std::move(output);
+		return exp;
+	}
+
+	const math::data_type* validate_expression(const math::expression& exp)
+	{
+		return nullptr;
+	}
+}
+
+/*
+=======================================
+	PARSING
 =======================================
 */
 
@@ -539,16 +851,73 @@ namespace matl_internal
 	{
 		size_t iterator = 0;
 		int line_conter = 0;
+
+		//std::vector<int> errors;
+
+		heterogeneous_map<std::string, typedefs::variable_definition, hgm_solver> variables;
+		heterogeneous_map<std::string, typedefs::property_definition, hgm_solver> properties;
 	};
 
 	void parse_line(const std::string& material_source, matl::context* context, parsing_state& state);
 
 	using keyword_handle = void(*)(const std::string& material_source, matl::context* context, parsing_state& state);
 
-	heterogeneous_map<std::string, keyword_handle, solver> keywords_handles =
+	namespace matl_keywords
+	{
+		void let(const std::string& source, matl::context* context, parsing_state& state)
+		{
+			auto& iterator = state.iterator;
+
+			get_spaces(source, iterator);
+			auto var_name = get_string_ref(source, iterator);
+
+			if (var_name.at(0) == builtins::symbols_prefix)
+				matl_throw(exception_type::symbol_declaration, {});
+
+			get_spaces(source, iterator);
+			auto assign_operator = get_char(source, iterator);
+
+			if (assign_operator != '=')
+				matl_throw(exception_type::invalid_syntax, {});
+
+			auto exp = get_expression(source, iterator);
+
+			typedefs::variable_definition var_def;
+			var_def.return_type = validate_expression(var_def.definition);
+			var_def.definition = std::move(exp);
+
+			state.variables.insert({ var_name, std::move(var_def) });
+		}
+
+		void property(const std::string& source, matl::context* context, parsing_state& state)
+		{
+
+		}
+
+		void _using(const std::string& source, matl::context* context, parsing_state& state)
+		{
+
+		}
+
+		void func(const std::string& source, matl::context* context, parsing_state& state)
+		{
+
+		}
+
+		void _return(const std::string& source, matl::context* context, parsing_state& state)
+		{
+
+		}
+	}
+
+	heterogeneous_map<std::string, keyword_handle, hgm_solver> keywords_handles =
 	{
 		{
-			{"let", nullptr}
+			{"let", matl_keywords::let},
+			{"property", matl_keywords::property},
+			{"using", matl_keywords::_using},
+			{"func", matl_keywords::func},
+			{"return", matl_keywords::_return}
 		}	
 	};
 }
@@ -568,7 +937,7 @@ matl::parsed_material matl::parse_material(const std::string& material_source, m
 	return {};
 }
 
-void matl_internal::parse_line(const std::string& material_source, matl::context* context, parsing_state& state)
+inline void matl_internal::parse_line(const std::string& material_source, matl::context* context, parsing_state& state)
 {
 	auto& source = material_source;
 	auto& iterator = state.iterator;
@@ -588,7 +957,7 @@ void matl_internal::parse_line(const std::string& material_source, matl::context
 		return;
 	}
 	
-	//kh_itr->second(material_source, context, state);
+	kh_itr->second(material_source, context, state);
 }
 
 /*
